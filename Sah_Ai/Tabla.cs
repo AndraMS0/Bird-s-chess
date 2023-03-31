7d1f0ebf67abe68391613b4484af719b8c509db5
Barzan Radu <barzanradu23@yahoo.com>
2023-03-31 11:40:35 +0300

    Moving images

--- Sah_Ai/Tabla.cs
+++ Sah_Ai/Tabla.cs
@@ -8,70 +8,94 @@ using System.Windows.Forms;
 
 namespace Sah_Ai
 {
-	public class Tabla
-	{
-		private TableLayoutPanel chessBoard;
+    public class Tabla
+    {
+        private TableLayoutPanel chessBoard;
 
-		public Tabla(Form form)
-		{
-			chessBoard = new TableLayoutPanel();
-			chessBoard.RowCount = 8;
-			chessBoard.ColumnCount = 10;
-			chessBoard.Dock = DockStyle.Fill;
-			chessBoard.Margin = new Padding(2);
+        public Tabla(Form form)
+        {
+            chessBoard = new TableLayoutPanel();
+            chessBoard.RowCount = 8;
+            chessBoard.ColumnCount = 10;
+            chessBoard.Dock = DockStyle.Fill;
+            chessBoard.Anchor = AnchorStyles.None;
+            chessBoard.Padding = chessBoard.Padding = new Padding(form.ClientSize.Width / 10, form.ClientSize.Width / 20, 0, 0);
+            chessBoard.Size = new Size((int)(form.ClientSize.Width * 0.9), (int)(form.ClientSize.Height * 0.9));
 
-			// Set the row and column styles
-			for (int i = 0; i < 8; i++)
-			{
-				chessBoard.RowStyles.Add(new RowStyle(SizeType.Percent, 12.5f));
-			}
-			for (int j = 0; j < 10; j++)
-			{
-				chessBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10f));
-			}
 
-			// Add the chess board squares to the table layout panel
-			for (int i = 0; i < 8; i++)
-			{
-				for (int j = 0; j < 10; j++)
-				{
-					Panel square = new Panel();
+            for (int i = 0; i < 8; i++)
+            {
+                chessBoard.RowStyles.Add(new RowStyle(SizeType.Percent, 12.5f));
+            }
+            for (int j = 0; j < 10; j++)
+            {
+                chessBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10f));
+            }
 
-					square.Dock = DockStyle.Fill;
-					square.Size = new Size(60, 60);
-					square.Padding = new Padding(5);
-					square.BackColor = Color.Green;
-					square.Visible = true;
-					if ((i + j) % 2 == 0)
-					{
-						square.BackColor = Color.FromArgb(238, 238, 238);
 
-					}
-					else
-					{
-						square.BackColor = Color.FromArgb(119, 136, 153);
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button square = new Button();
+                    square.Dock = DockStyle.Fill;
+                    square.Size = new Size(70, 70);
+                    square.Margin = new Padding(0);
+                    square.Padding = new Padding(0);
+                    square.Visible = true;
+                    square.Click += Square_Click;
+                    if ((i + j) % 2 == 0)
+                    {
+                        square.BackColor = Color.White;
+                    }
+                    else
+                    {
+                        square.BackColor = Color.FromArgb(160, 160, 160);
+                    }
+                    chessBoard.Controls.Add(square, j, i);
+                }
+            }
 
-					}
-					chessBoard.Controls.Add(square, j, i);
 
-				}
-			}
+            form.Controls.Add(chessBoard);
+        }
 
-			// Add the table layout panel to the form
-			form.Controls.Add(chessBoard);
-		}
-		public void AdaugaPiesa(int row, int col, string imageFileName)
-		{
-			// Get the panel that represents the square
-			Panel square = (Panel)chessBoard.GetControlFromPosition(row, col);
+        private void Square_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int currentRow = chessBoard.GetRow(clickedButton);
+            int currentCol = chessBoard.GetColumn(clickedButton);
+            int nextRow = currentRow + 1;
 
-			// Create a new PictureBox control and set its image
-			PictureBox piece = new PictureBox();
-			piece.Image = Image.FromFile(imageFileName);
-			piece.SizeMode = PictureBoxSizeMode.Zoom;
+            if (nextRow < 8)
+            {
+                Button nextButton = (Button)chessBoard.GetControlFromPosition(currentCol, nextRow);
 
-			// Add the PictureBox control to the square
-			square.Controls.Add(piece);
-		}
-	}
+                if (nextButton.BackgroundImage != null)
+                {
+                    return;
+                }
+                else
+                {
+                    nextButton.BackgroundImage = clickedButton.BackgroundImage;
+                    clickedButton.BackgroundImage = null;
+                }
+            }
+        }
+
+        public void AddPieceToButton(string filePath, int row, int col)
+        {
+            Button button = (Button)chessBoard.GetControlFromPosition(col, row);
+            Image image = Image.FromFile(filePath);
+
+            // resize the image to fit the button
+            image = new Bitmap(image, button.Size);
+
+            // center the image in the button
+            button.BackgroundImageLayout = ImageLayout.Center;
+
+            // set the image as the button's background image
+            button.BackgroundImage = image;
+        }
+    }
 }
